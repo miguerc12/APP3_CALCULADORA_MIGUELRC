59986b89c37bf81789d113c56646a7d912a4261b
miguerc12 <cobmigue329@gmail.com>
2024-03-09 12:04:22 -0600

    añadi la interfaz de usuario en un oncreate

--- app/src/main/java/rodriguez/miguel/app3_calculadora_miguel_rc/MainActivity.java
+++ app/src/main/java/rodriguez/miguel/app3_calculadora_miguel_rc/MainActivity.java
@@ -1,14 +1,32 @@
 package rodriguez.miguel.app3_calculadora_miguel_rc;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.os.Bundle;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    TextView display;
+    TextView resultDisplay;
+    String currentNumber = "";
+    String currentExpression = "";
+    double result = 0;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        display = findViewById(R.id.display);
+        resultDisplay = findViewById(R.id.result_display);
     }
 }
+
+
+
